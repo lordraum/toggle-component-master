8009d46aaf4f4bd619098b5a6919ba9df1f966fb
David Gomez <lordraum@gmail.com>
2023-03-08 10:29:06 -0500

    grid layout, flex card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,11 +6,25 @@ import CardBenefit from "./CardBenefit";
 const Cards = () => {
   return (
     <section className="cards">
-      <Card title="Tarjeta1" price="0">
+      <Card title="Basic" price="$19.99">
         <CardBody>
-          <CardBenefit benefit="asdfasdf" />
-          <CardBenefit benefit="asdfasdf" />
-          <CardBenefit benefit="asdfasdf" />
+          <CardBenefit benefit="500 GB Storage" />
+          <CardBenefit benefit="2 Users Allowed" />
+          <CardBenefit benefit="Send up to 3 GB" />
+        </CardBody>
+      </Card>
+      <Card title="Basic" price="$19.99">
+        <CardBody>
+          <CardBenefit benefit="500 GB Storage" />
+          <CardBenefit benefit="2 Users Allowed" />
+          <CardBenefit benefit="Send up to 3 GB" />
+        </CardBody>
+      </Card>
+      <Card title="Basic" price="$19.99">
+        <CardBody>
+          <CardBenefit benefit="500 GB Storage" />
+          <CardBenefit benefit="2 Users Allowed" />
+          <CardBenefit benefit="Send up to 3 GB" />
         </CardBody>
       </Card>
     </section>
