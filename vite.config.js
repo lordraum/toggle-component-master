cbf7228ddfcfa64da48291e40bbf5cf21f85a88e
David Gomez <lordraum@gmail.com>
2023-03-11 11:27:04 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontendmentor-toggle-master-card/",
+  base: "/toggle-component-master/",
 });
